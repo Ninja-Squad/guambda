0fa026c40f91c65426d15b99feaad1f04a8c7ac4
JB Nizet <jnizet@gmail.com>
2012-12-10 07:58:26 +0100

    Improve Sort by making it Sort<T> rather than Sort<T, R>

--- src/main/java/com/ninja_squad/guambda/Sort.java
+++ src/main/java/com/ninja_squad/guambda/Sort.java
@@ -4,23 +4,23 @@ import java.util.*;
 import java.util.functions.*;
 import com.google.common.collect.*;
 
-public class Sort<T, R> implements Comparator<T> {
-    private Mapper<R, T> mapper;
-    private Comparator<? super R> comparator;
-    private Sort<T, ?> previous;
+public final class Sort<T> implements Comparator<T> {
+    private Mapper<?, T> mapper;
+    private Comparator<?> comparator;
+    private Sort<T> previous;
     
-    private Sort(Mapper<R, T> mapper, Comparator<? super R> comparator, Sort<T, ?> previous) {
+    private Sort(Mapper<?, T> mapper, Comparator<?> comparator, Sort<T> previous) {
         this.previous = previous;
         this.mapper = mapper;
         this.comparator = comparator;
     }
     
-    public static <T, R extends Comparable<? super R>> Sort<T, R> by(Mapper<R, T> mapper) {
+    public static <T, R extends Comparable<? super R>> Sort<T> by(Mapper<R, T> mapper) {
         Objects.requireNonNull(mapper);
         return new Sort(mapper, Ordering.natural(), null);
     }
     
-    public static <T, R> Sort<T, R> by(Mapper<R, T> mapper, Comparator<? super R> comparator) {
+    public static <T, R> Sort<T> by(Mapper<R, T> mapper, Comparator<? super R> comparator) {
         Objects.requireNonNull(mapper);
         Objects.requireNonNull(comparator);
         return new Sort(mapper, comparator, null);
@@ -32,14 +32,18 @@ public class Sort<T, R> implements Comparator<T> {
         if (previous != null) {
             result = previous.compare(o1, o2);
         }
-        return result == 0 ? comparator.compare(mapper.map(o1), mapper.map(o2)) : result;
+        if (result == 0) {
+            // using cast here, but type safety is ensured by the factory methods
+            result = ((Comparator<Object>) comparator).compare(mapper.map(o1), mapper.map(o2));
+        }
+        return result;
     }
     
-    public <R2 extends Comparable<? super R2>> Sort<T, R2> thenBy(Mapper<R2, T> mapper) {
+    public <R extends Comparable<? super R>> Sort<T> thenBy(Mapper<R, T> mapper) {
         return new Sort(mapper, Ordering.natural(), this);
     }
     
-    public <R2> Sort<T, R2> thenBy(Mapper<R2, T> mapper, Comparator<? super R2> comparator) {
+    public <R> Sort<T> thenBy(Mapper<R, T> mapper, Comparator<? super R> comparator) {
         return new Sort(mapper, comparator, this);
     }
     
